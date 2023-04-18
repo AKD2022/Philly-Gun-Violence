009c80532d1f8260e3976030d13614bb7f7b71f6
AKD <akdusa20@gmail.com>
2023-04-18 16:13:28 -0400

    commit

--- statistics.js
+++ statistics.js
@@ -1,1 +1,21 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
 
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active')
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}  
+
+ScrollReveal({ 
+    reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+});
+
+ScrollReveal().reveal('.main-title', { delay: 500 });
