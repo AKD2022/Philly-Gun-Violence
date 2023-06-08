f0d6f013f5bf2bf8180e14e6b0f832e7e312fe50
AKD <akdusa20@gmail.com>
2023-06-07 21:57:22 -0400

    used session storage, edited part of statistics starting video

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,4 @@
 const splash = document.querySelector('.splash');
-
 document.addEventListener('DOMContentLoaded', (e) => {
   setTimeout(() => {
     splash.classList.add('display-none');
@@ -8,21 +7,21 @@ document.addEventListener('DOMContentLoaded', (e) => {
 
 
 var firstContainer = document.querySelector(".splash");
+var already_splashed = document.querySelector(".btn");
+var already_splashed2 = document.querySelector(".details h1");
+var already_splashed3 = document.querySelector(".details p");
+var bg_already_splashed = document.querySelector(".container")
 
-var result = localStorage.getItem('hasRan');
+var result = sessionStorage.getItem('hasRan');
 
 if (!result) {
-        localStorage.setItem('hasRan', true);
-} else if (window.onclose) {
-    firstContainer.classList.remove("post_animated");
-    firstContainer.classList.add("animated");
-    localStorage.setItem('hasRan', false);
+        sessionStorage.setItem('hasRan', true);
 } else {
     firstContainer.classList.remove("animated");
     firstContainer.classList.add("post_animated");
+    already_splashed.classList.add("fade-in-already");
+    already_splashed2.classList.add("fade-in-already2");
+    already_splashed3.classList.add("fade-in-already3");
+    bg_already_splashed.classList.add("bg_already_splashed");
 }
-
-
-
-
 
