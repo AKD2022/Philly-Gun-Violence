f7ae2671ce50c32c1a917ab6c068bffb4be0f2a1
AKD <akdusa20@gmail.com>
2023-06-25 20:49:16 -0400

    started idea

--- JS/where.js
+++ JS/where.js
@@ -54,33 +54,5 @@ const observer1 = new IntersectionObserver((entries1) => {
 const hiddenElements1 = document.querySelectorAll('.gonorth-content');
 hiddenElements1.forEach((el) => observer1.observe(el));
 
-/* Dropdown */
 
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
 
-/* Dive North */
-/* Dive North End */
-
-/* Dive South */
-/* Dive South End */
-
-/* Dive West */
-/* Dive West End */
