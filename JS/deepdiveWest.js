71314ef6462213bd0ab87861c7dd3d4e829778e4
AKD <akdusa20@gmail.com>
2023-07-05 10:55:13 -0400

    deleted files

--- JS/deepdiveWest.js
+++
@@ -1,7 +0,0 @@
-var dw = L.map('dw-map').setView([39.985049, -75.137209], 11);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 12,
-    minZoom: 11, 
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(dw);
