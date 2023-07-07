a81defbf2c5a577b46509d9e95e64471733931f6
AKD <akdusa20@gmail.com>
2023-07-07 11:38:32 -0400

    created scroll trigger

--- JS/where.js
+++ JS/where.js
@@ -1,4 +1,24 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const tl = gsap.timeline();
+
+tl.to(".wrapper", 5, {x:-window.innerWidth})
+.from (".section2 h2", 5, {})
+
+.to(".wrapper", 5, {x:-window.innerWidth*2})
+.from (".section3 h2", 5, {})
+
+.to(".wrapper", 5, {x:-window.innerWidth*3})
+.from (".section4 h2", 5, {})
+
+ScrollTrigger.create({
+    animation: tl, 
+    trigger: ".wrapper",
+    start: "center center",
+    end: "+=4000",
+    scrub: true, 
+    pin:true 
+})  
 
 
-
-
+ 
